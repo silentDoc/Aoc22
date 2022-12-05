c3d7c7b5fcdcd8eaeb23d26ef1b3da4bfcee31d3
SilentDoc <rfelip@outlook.es>
2022-12-06 00:17:47 +0100

    Day 1 - Part 1 - Complete

--- Aoc22/Aoc22/Aoc22/Program.cs
+++ Aoc22/Aoc22/Aoc22/Program.cs
@@ -1,19 +1,56 @@
-﻿namespace Aoc22
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aoc22
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Program _instance = new Program();
-            _instance.Day1();
-            
+            int day = 1;
+
+            switch (day)
+            {
+                case 1:
+                    //string input = "./Input/day1_1_test.txt";
+                    string input = "./Input/day1_1.txt";
+                    var result = _instance.Day1(input);
+                    Console.WriteLine("Result : {0}", result);
+                    
+                    break;
+                default:
+                    Console.WriteLine("Bad day ;)");
+                    break;
+            }
+            Console.WriteLine("Key to exit");
+            Console.ReadLine();
         }
 
-        void Day1()
+        int Day1(string input)
         {
-            var lines = File.ReadLines("./Input/day1_1_test.txt");
-            lines.ToList<string>().ForEach(x => Console.WriteLine(x));
-            Console.ReadLine();
+            List<Elf> elves = new();
+
+            var lines = File.ReadLines(input).ToList();
+            
+            var i = 0;
+            
+            Elf currentElf = new();
+            while (i < lines.Count)
+            {
+                if (string.IsNullOrEmpty(lines[i]))
+                {
+                    elves.Add(currentElf);
+                    currentElf = new();
+                    i++;
+                    continue;
+                }
+
+                currentElf.AddCalories(int.Parse(lines[i]));
+                i++;
+            }
+
+            var maxCal = elves.Max(x => x.GetCaloriesCount());
+            return maxCal;
 
         }
     }
