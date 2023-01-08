69c1b687844ebc1fc7730f9ee062899ebe9a8e86
silentDoc <ramon78t@yahoo.es>
2023-01-08 22:30:13 +0100

    Added MathUtil class to reuse GCD and LCM

---
+++ Aoc22/Aoc22/Aoc22/Common/MathUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc22.Common
+{
+    static class MathUtils
+    {
+        static int GCD(int x, int y) 
+        {
+            int gcd = 0;
+            int a = Math.Max(x, y);
+            int b = Math.Min(x, y);
+            do
+            {
+                gcd = b;
+                b = a % b;
+                a = gcd;
+            } while(b!=0);
+            return gcd;
+        }
+
+        static int LCM(int x, int y)
+        {
+            int a = Math.Max(x, y);
+            int b = Math.Min(x, y);
+            return (a / GCD(a, b)) * b;
+        }
+
+    }
+}
